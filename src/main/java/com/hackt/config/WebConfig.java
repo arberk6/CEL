723a48af140750395370706699d4556cb677985a
arberk6 <kadriu.arber1@gmail.com>
2019-02-02 01:02:30 +0100

    update, error solved

---
+++ src/main/java/com/hackt/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.hackt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public InternalResourceViewResolver resolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/pages/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    public void configureDefaulServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
+
+}
